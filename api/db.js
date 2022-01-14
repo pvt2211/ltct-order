594792918e37668c84d2fa07471b95188758bbd7
pvt2211 <48007515+pvt2211@users.noreply.github.com>
2022-01-15 02:59:33 +0700

    hot fix

--- api/db.js
+++ api/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: `remotemysql.com`,
+  port: `3306`,
+  user: `BE9uYnNnuY`,
+  password: `qTKEMEKs1T`,
+  database: `BE9uYnNnuY`
 });
 
 module.exports = db
